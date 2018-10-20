c836c09adf3c1d253ad657d5a6be3b4c30c6d7bd
fyxxf <44317465+fyxxf@users.noreply.github.com>
2018-10-20 16:28:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const bot = new Discord.Client();
-
-const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
-
-client.on("ready", () => {
-  console.log("Bağlandım!")   
-});
 
 client.on('ready', () => {
-    client.user.setStatus('available')
-    client.user.setPresence({
-        game: {
-            name: 'f!yardım Bot aktif.',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
-    });
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
